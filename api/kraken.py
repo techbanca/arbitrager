65a355075704c93f944a1e1a626f6515fdd38117
BancaOfficial <35514940+BancaOfficial@users.noreply.github.com>
2018-05-28 21:10:39 -0700

    Update kraken.py

--- api/kraken.py
+++ api/kraken.py
@@ -19,3 +19,12 @@ def topAskBid(pair):
     bid = float(orders['bids'][0][0])
     return ask, bid
 
+def top5(pair):
+    formattedAsks, formattedBids = [], []
+    orders = getOrders(pair, '5')['result'][pair]
+    asks, bids = orders['asks'], orders['bids']
+    for ask in asks:
+        formattedAsks.append((float(ask[0]), float(ask[1]), 'kraken'))
+    for bid in bids:
+        formattedBids.append((float(bid[0]), float(bid[1]), 'kraken'))
+    return formattedAsks, formattedBids

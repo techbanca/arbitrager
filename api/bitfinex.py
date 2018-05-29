81b49b37b6c48b54fdd24235010bd775f1388732
Banca <38743943+techbanca@users.noreply.github.com>
2018-05-28 21:07:24 -0700

    Update bitfinex.py

--- api/bitfinex.py
+++ api/bitfinex.py
@@ -9,3 +9,8 @@ def getOrders(pair):
     r = requests.get('https://api.bitfinex.com/v1/book/'+pair)
     return r.json()
 
+def topAskBid(pair):
+    orders = getOrders(pair)
+    ask = float(orders['asks'][0]['price'])
+    bid = float(orders['bids'][0]['price'])
+    return ask, bid

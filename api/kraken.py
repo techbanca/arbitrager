282af44d78508c1d52b44fd3a9f3c95f4ce1c568
Banca <38743943+techbanca@users.noreply.github.com>
2018-06-02 21:44:02 -0700

    Update kraken.py

--- api/kraken.py
+++ api/kraken.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 
-
 @documentation: https://www.kraken.com/en-us/help/api#general-usage
 """
 import requests
@@ -22,15 +21,13 @@ def topAskBid(pair):
 
 def top5(pair):
     formattedAsks, formattedBids = [], []
-    orders = getOrders(pair, '5')['result'][pair]
+    orders = getOrders(pair, '6')['result'][pair]
     asks, bids = orders['asks'], orders['bids']
     for ask in asks:
         formattedAsks.append((float(ask[0]), float(ask[1]), 'kraken'))
     for bid in bids:
         formattedBids.append((float(bid[0]), float(bid[1]), 'kraken'))
     return formattedAsks, formattedBids
-
-
 
 """#x = requests.get('https://api.kraken.com/0/public/AssetPairs').json()['result']
 xkeys = ['DASHEUR', 'DASHUSD', 'DASHXBT', 'EOSETH', 'EOSEUR', 'EOSUSD', 'EOSXBT', 'GNOETH', 'GNOEUR', 'GNOUSD', 'GNOXBT', 'USDTZUSD', 'XETCXETH', 'XETCXXBT', 'XETCZEUR', 'XETCZUSD', 'XETHXXBT', 'XETHXXBT.d', 'XETHZCAD', 'XETHZCAD.d', 'XETHZEUR', 'XETHZEUR.d', 'XETHZGBP', 'XETHZGBP.d', 'XETHZJPY', 'XETHZJPY.d', 'XETHZUSD', 'XETHZUSD.d', 'XICNXETH', 'XICNXXBT', 'XLTCXXBT', 'XLTCZEUR', 'XLTCZUSD', 'XMLNXETH', 'XMLNXXBT', 'XREPXETH', 'XREPXXBT', 'XREPZEUR', 'XREPZUSD', 'XXBTZCAD', 'XXBTZCAD.d', 'XXBTZEUR', 'XXBTZEUR.d', 'XXBTZGBP', 'XXBTZGBP.d', 'XXBTZJPY', 'XXBTZJPY.d', 'XXBTZUSD', 'XXBTZUSD.d', 'XXDGXXBT', 'XXLMXXBT', 'XXLMZEUR', 'XXLMZUSD', 'XXMRXXBT', 'XXMRZEUR', 'XXMRZUSD', 'XXRPXXBT', 'XXRPZCAD', 'XXRPZEUR', 'XXRPZJPY', 'XXRPZUSD', 'XZECXXBT', 'XZECZEUR', 'XZECZUSD']
